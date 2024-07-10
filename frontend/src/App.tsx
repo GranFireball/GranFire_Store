3dc627169dc10efabeaebc9822a9c7405c025a89
GranFireball <leonardohk10@hotmail.com>
2024-07-10 20:34:19 -0300

    feat: create routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/header'
 import ListSaleProducts from './components/listSaleProducts'
+import Cart from './pages/cart'
+import ListProducts from './components/listProducts'
 
 function App() {
   return (
-    <>
-      <Header/>
-      <ListSaleProducts/>
-    </>
+    <Routes>
+      <Route path="/">
+        <Route index element={<>
+            <Header/>
+            <ListSaleProducts/>
+            <ListProducts/>
+          </>}/>
+        <Route path="cart" element={<Cart/>}/>
+      </Route>
+    </Routes>
   )
 }
 
