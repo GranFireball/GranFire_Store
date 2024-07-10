f6840eb69d4ceabd6bbcd9e2caa3f55a027db110
GranFireball <leonardohk10@hotmail.com>
2024-07-10 20:34:48 -0300

    feat: sign up google

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,27 +2,75 @@ import './header.css'
 import imgLogo from "../imgs/logo.png"
 import { CgProfile } from "react-icons/cg";
 import { MdLogout } from "react-icons/md";
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import { BsFillCartFill } from 'react-icons/bs';
 
-export default function Header(){
-  const [logged, setLogged] = useState<string | undefined>();
+export default function Header() {
+  const [accessToken, setAccessToken] = useState<string>();
+  const [profile, setProfile] = useState<any>([]);
   const IconSize = 24;
-  return(
-    <header className="headerContainer">
-      <img src={imgLogo} alt="Logo GranFire Store" className="imgLogo"/>
+
+  useEffect(
+    () => {
+      if (accessToken) {
+        fetch(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            Accept: 'application/json'
+          }
+        })
+          .then((data) => data.json())
+          .then((res) => {
+            setProfile(res);
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [accessToken]
+  );
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setAccessToken(codeResponse.access_token),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
+
+  function logout() {
+    googleLogout();
+    setAccessToken(undefined);
+  }
+
+  return (
+    <header className="headerSticky">
+      <div className="headerContainer">
+        <img src={imgLogo} alt="Logo GranFire Store" className="imgLogo" />
+        {
+          accessToken ?
+            <Fragment>
+              <div className="loginAndLogoutButton" onClick={() => logout()}>
+                <span>Sair</span>
+                <MdLogout size={IconSize} />
+              </div>
+              {
+                profile &&
+                <p className="profileName">Conta de: {profile.name}</p>  
+              }
+            </Fragment>
+            :
+            <div className="loginAndLogoutButton" onClick={() => login()}>
+              <span>Entrar</span>
+              <CgProfile size={IconSize} />
+            </div>
+        }
+      </div>
       {
-        logged ?
-        <Fragment>
-          <div className="loginAndLogoutButton" onClick={() => setLogged(undefined)}>
-            <span>Sair</span>
-            <MdLogout size={IconSize}/>
+        profile.name &&
+        <div className="profileCartContainer">
+          <div className="profileCart">
+            <p>Seu Carrinho</p>
+            <BsFillCartFill size={IconSize}/>
           </div>
-          <p className="loggedUser">Conta de: {logged}</p>
-        </Fragment>
-        :
-        <div className="loginAndLogoutButton" onClick={() => setLogged("Gran Firebalaaaaaaaaaal")}>
-          <span>Entrar</span>
-          <CgProfile size={IconSize}/>
         </div>
       }
     </header>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="379254638514-1oqihgqmb85qnsbo9mqcst2012gc2dr1.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
